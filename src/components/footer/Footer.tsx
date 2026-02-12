01e760a42f4d73912c95d2f1266561a64fdd9fb8
Cesar Henrique <dev.cesarhenrique@gmail.com>
2026-02-12 12:03:27 -0300

    Foi criado Tela de Cadastro e Login e suas respectivas rotas de acesso

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -14,9 +14,15 @@ function Footer() {
             </p>
             <p className="text-lg">Acesse nossas redes sociais</p>
             <div className="flex gap-2">
-                <LinkedinLogoIcon size={48} weight="bold" />
-                <InstagramLogoIcon size={48} weight="bold" />
-                <GithubLogoIcon size={48} weight="bold" />
+                <a href="https://www.linkedin.com/in/cesarhennrique/" target="_blank">
+                    <LinkedinLogoIcon size={48} weight="bold" />
+                </a>
+                <a href="https://www.instagram.com/dev.cesarhenrique/" target="_blank">
+                    <InstagramLogoIcon size={48} weight="bold" />
+                </a>
+                <a href="https://github.com/cesarhennrique" target="_blank">
+                    <GithubLogoIcon size={48} weight="bold" />
+                </a>
 
             </div>
 
